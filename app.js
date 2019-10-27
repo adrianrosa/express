dd3222e2847a5fb9a15df0f683b040664aef8e6a
adrianrosa <adrian.rosa@dafiti.com.ar>
2019-10-26 23:37:10 -0300

    Initial commits

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var service = require('./service');
+const fetch = require('node-fetch');
+var cors = require('cors');
+
+app.get('/movies', (req, res) => {
+   res.json(service.getMovies());
+   /*fetch('http://rockefeller.local:8081/api/installments', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',  
+          'Authorization': 'Bearer Y2JkNjlkNTQ1YzcxNDhjYjQxMzhjNDJhMDBiODAxYzUxYTY2OTA0M2FiZmZlMTc3YzE1MTNhMDc0ODQwODFlZQ',
+          'Accept': 'application/json'
+        },
+    })
+    .then(resp => resp.json())
+    .then(json => res.json(json));*/
+});
+
+app.use(cors());
+
+app.listen(3501, () => {
+   console.log('API listening on port 3501');
+});
---
+++ service.js
@@ -0,0 +1,13 @@
+module.exports = {
+    getMovies: () => ({
+      "title": "The Basics - Networking",
+      "description": "Your app fetched this form a remote endpoint!",
+      "movies": [
+        {"id": "1", "title": "Star Wars", "releaseYear": "1977"},
+        {"id": "2", "title": "Back to the Future", "releaseYear": "1985"},
+        {"id": "3", "title": "The Matrix", "releaseYear": "1999"},
+        {"id": "4", "title": "Inception", "releaseYear": "2010"},
+        {"id": "5", "title": "Interstellar", "releaseYear": "2014"}
+      ]
+    })
+};
